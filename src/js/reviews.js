9082621b6f329ae14c478eb534177a231cdfb989
shalfey41 <bezu-dima2250@yandex.ru>
2016-10-31 22:21:59 +0300

    Показала фильтры, добаил класс для рейтинга, очистил src картинки при ошибке.

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -21,9 +21,9 @@ window.reviewList = (function() {
       var reviews = data;
 
       if (reviews.length > 0) {
-        var reviewListElement = document.querySelector('.reviews-list');
+        this.showFilters();
 
-        this.hideFilters();
+        var reviewListElement = document.querySelector('.reviews-list');
 
         reviews.forEach(function(item) {
           reviewListElement.appendChild(reviewList.createReview(item));
@@ -56,20 +56,60 @@ window.reviewList = (function() {
     },
 
     /**
+     * Появление блока с фильтрами
+     */
+    showFilters: function() {
+      var filters = document.querySelector('.reviews-filter');
+
+      filters.classList.remove('invisible');
+    },
+
+    /**
      * Создание отзыва
      * @param {Object} reviewItemData данные по отзыву
      * @returns {Element}
      */
     createReview: function(reviewItemData) {
       var reviewElement = templateContainer.querySelector('.review').cloneNode(true);
-      var reviewRating = reviewElement.querySelector('.review-rating');
       var reviewText = reviewElement.querySelector('.review-text');
+      var reviewRating = reviewElement.querySelector('.review-rating');
 
       this.fillImage(reviewElement, reviewItemData);
-      reviewRating.textContent = reviewItemData.rating;
+      reviewRating.classList.add(this.getRatingClass(reviewItemData.rating));
       reviewText.textContent = reviewItemData.description;
 
       return reviewElement;
+    },
+
+    /**
+     * Получение класса для рейтинга
+     * @param {Number} stars
+     * @returns {String}
+     */
+    getRatingClass: function(stars) {
+      var className = '';
+
+      switch (stars) {
+        case 1:
+          className = 'review-rating-one';
+          break;
+        case 2:
+          className = 'review-rating-two';
+          break;
+        case 3:
+          className = 'review-rating-three';
+          break;
+        case 4:
+          className = 'review-rating-four';
+          break;
+        case 5:
+          className = 'review-rating-five';
+          break;
+        default:
+          break;
+      }
+
+      return className;
     },
 
     /**
@@ -97,11 +137,13 @@ window.reviewList = (function() {
       reviewImage.title = reviewData.author.name;
 
       imageTimeout = setTimeout(function() {
+        image.src = '';
         reviewContainer.classList.add('review-load-failure');
       }, IMAGE_LOAD_TIMEOUT);
     }
   };
 
+  reviewList.hideFilters();
   reviewList.getReviewsData(DATA_URL, JSONP_FUNCTION_NAME);
 
   return reviewList;
