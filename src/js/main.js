3f88611bda42f53758847048640c02f6679bef39
shalfey41 <bezu-dima2250@yandex.ru>
2016-11-04 22:19:02 +0300

    Написал конструктор Gallery

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
   var Game = require('./game');
   var form = require('./form');
   var reviews = require('./reviews');
+  var Gallery = require('./gallery');
 
   var game = new Game(document.querySelector('.demo'));
 
@@ -38,4 +39,33 @@
   window.renderReviews = function(data) {
     reviews.render(data);
   };
+
+  /**
+   * Галерея
+   */
+  var imagesContainers = document.querySelectorAll('.photogallery-image');
+  var pictures = [];
+
+  // В массив pictures записываем ссылки на картинки
+  Array.prototype.forEach.call(imagesContainers, function(container) {
+    pictures.push(container.querySelector('img').src);
+  });
+
+  var gallery = new Gallery(pictures);
+
+  // Обработчик на каждый контейнер с картинкой
+  for (var i = 0; i < imagesContainers.length; i++) {
+    imagesContainers[i].onclick = function() {
+      var currentImage = this.querySelector('img');
+      var pictureNumber = null;
+
+      pictures.forEach(function(imageSrc, index) {
+        if (imageSrc === currentImage.src) {
+          pictureNumber = index;
+        }
+      });
+
+      gallery.show(pictureNumber);
+    };
+  }
 })();
