92bd87a62492519bbb3dadee4a726dd7cfc4c89a
shalfey41 <bezu-dima2250@yandex.ru>
2016-11-13 13:07:33 +0300

    Добавил фильтрацию отзывов и загрузку данных через XMLHttpRequest

--- src/js/load.js
+++ src/js/load.js
@@ -4,12 +4,44 @@
 
 'use strict';
 
+var getPage = require('../../bin/data/get-page');
+var filter = require('../../bin/data/filter');
+
+var moreReviewsBtn = document.querySelector('.reviews-controls-more');
+
 /**
- * Отправка JSONP запроса
+ * Отправка XMLHttpRequest запроса
  * @param {String} url адрес запроса
+ * @param {Object} params параметры запроса
+ * @param {Function} callback
  */
-module.exports = function(url) {
-  var script = document.createElement('script');
-  script.src = url + '?callback=renderReviews';
-  document.body.appendChild(script);
+module.exports = function(url, params, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url + '?' + JSON.stringify(params));
+
+  xhr.onload = function(e) {
+    var request = JSON.parse(e.currentTarget.response);
+
+    // Фильтрация отзыва
+    var filteredList = filter(request, params.filter);
+
+    // Если есть еще отзывы, показать кнопку «Ещё отзывы»
+    if (filteredList.length > params.to) {
+      moreReviewsBtn.classList.remove('invisible');
+    } else {
+      moreReviewsBtn.classList.add('invisible');
+    }
+
+    // Получение нужной страницы
+    var data = getPage(filteredList, params.from, params.to);
+
+    callback(data);
+  };
+
+  xhr.onerror = function() {
+    console.log('Ошибка ' + this.status);
+  };
+
+  xhr.send();
 };
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -11,13 +11,20 @@ var filters = require('./filters');
 module.exports = (function() {
   var DATA_URL = 'http://localhost:1507/api/reviews';
 
+  var paramsToLoad = {
+    from: 0,
+    to: 3,
+    filter: 'reviews-all'
+  };
+
   var reviews = {
     /**
      * Загрузка данных по отзывам
      */
     loadReviews: function() {
       filters.hideFilters();
-      load(DATA_URL);
+
+      load(DATA_URL, paramsToLoad, this.render);
     },
 
     /**
@@ -35,8 +42,51 @@ module.exports = (function() {
           reviewListElement.appendChild(review);
         });
       }
+    },
+
+    /**
+     * Удаление списка отзывов
+     */
+    remove: function() {
+      var reviewListElement = document.querySelector('.reviews-list');
+
+      reviewListElement.innerHTML = '';
+
+      paramsToLoad.from = 0;
     }
   };
+
+  /**
+   * Событие загрузки отзывов
+   * @type {Element}
+   */
+  var moreReviewsBtn = document.querySelector('.reviews-controls-more');
+
+  moreReviewsBtn.addEventListener('click', function() {
+    var pageDifference = paramsToLoad.to - paramsToLoad.from;
+
+    paramsToLoad.from += pageDifference;
+    paramsToLoad.to += pageDifference;
+
+    load('/api/reviews', paramsToLoad, reviews.render);
+  });
+
+  /**
+   * Событие фильтрации
+   * @type {Element}
+   */
+  var filterList = document.querySelector('.reviews-filter');
+
+  filterList.addEventListener('change', function(e) {
+    var target = e.target;
+
+    if (target.name === 'reviews') {
+      reviews.remove();
+
+      paramsToLoad.filter = target.id;
+      load('/api/reviews', paramsToLoad, reviews.render);
+    }
+  }, true);
 
   return reviews;
 })();
